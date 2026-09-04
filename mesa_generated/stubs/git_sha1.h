33d1a808653adbbbc0e84865952a2e5e40560214
Michael Cox <michael@coxmichael.co.uk>
2026-09-04 18:04:41 +0100

    Add mesa 24.3.4

---
+++ mesa_generated/stubs/git_sha1.h
@@ -0,0 +1,4 @@
+#ifndef GIT_SHA1_H
+#define GIT_SHA1_H
+#define MESA_GIT_SHA1 "mesa-24.3.4"
+#endif
---
+++ mesa_generated/stubs/malloc.h
@@ -0,0 +1,7 @@
+#ifndef MALLOC_H_STUB
+#define MALLOC_H_STUB
+#include <stdlib.h>
+#ifndef alloca
+#define alloca __builtin_alloca
+#endif
+#endif
